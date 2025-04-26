0c7ff68f27e2186320116a2d3c8efa8ceb09983b
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-04-26 10:26:36 +0700

    Update CommunityPage styling: adjust banner text size and video icon dimensions for improved visual consistency.

--- src/app/dashboard/teens/community/page.tsx
+++ src/app/dashboard/teens/community/page.tsx
@@ -12,8 +12,8 @@ export default function CommunityPage() {
 
 const Banner = () => {
   return (
-    <div className="w-full h-20 bg-primary-blue align-middle rounded-lg flex items-center text-5xl gap-3 justify-center text-white font-bold">
-      <VideoIcon size={65} fill="#5DADFE" className="text-[#5DADFE]" />
+    <div className="w-full h-20 bg-primary-blue align-middle rounded-lg flex items-center text-4xl gap-3 py-4 justify-center text-white font-bold">
+      <VideoIcon size={50} fill="#5DADFE" className="text-[#5DADFE]" />
       <p className="align-middle inline-block">Community Library</p>
     </div>
   );
